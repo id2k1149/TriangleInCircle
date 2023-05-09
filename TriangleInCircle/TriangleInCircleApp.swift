adeb25ff4c2863747be71c7015e860b67fb7c8da
id2k1149 <id2k1149@gmail.com>
2023-05-08 20:11:50 -0400

    Initial Commit

---
+++ TriangleInCircle/TriangleInCircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriangleInCircleApp.swift
+//  TriangleInCircle
+//
+//  Created by Max Franz Immelmann on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TriangleInCircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
